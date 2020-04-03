d494c28528e9a25a139e1f75c86980894eb58f4c
anmol <anmol.singh@feryservices.com>
2020-04-03 23:18:00 +0530

    fresh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import './App.css';
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
+    <div>
+      <Layout>
+        <BurgerBuilder />
+      </Layout>
     </div>
   );
 }
