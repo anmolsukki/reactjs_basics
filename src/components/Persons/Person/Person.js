20c24fff5db95d8432ec773a9fa1dae9b4b987c2
anmolsukki <anmolsukki94@gmail.com>
2020-04-13 16:46:54 +0530

    wrapper component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import Aux from "../../../hoc/Aux"
 import classes from './Person.css';
 
 const person = props => {
@@ -12,6 +13,38 @@ const person = props => {
       <input type="text" onChange={props.changed} value={props.name} />
     </div>
   );
+
+  // ****************** OR ************************
+
+    // return [
+    //   <p key="1" onClick={props.click}>
+    //     I'm {props.name} and I am {props.age} years old!
+    //   </p>,
+    //   <p key="2">{props.children}</p>,
+    //   <input key="3" type="text" onChange={props.changed} value={props.name} />
+    // ];
+
+  // ****************** OR ************************
+      // return (
+      //   <Aux>
+      //     <p onClick={props.click}>
+      //       I'm {props.name} and I am {props.age} years old!
+      //     </p>
+      //     <p>{props.children}</p>
+      //     <input type="text" onChange={props.changed} value={props.name} />
+      //   </Aux>
+      // );
+
+  // ****************** OR ************************
+      // return (
+      //   <React.Fragment>
+      //     <p onClick={props.click}>
+      //       I'm {props.name} and I am {props.age} years old!
+      //     </p>
+      //     <p>{props.children}</p>
+      //     <input type="text" onChange={props.changed} value={props.name} />
+      //   </React.Fragment>
+      // );
 };
 
 export default person;
---
+++ src/hoc/Aux.js
@@ -0,0 +1,5 @@
+// Windows User file name must be [ Auxiliary.js ]
+
+const aux = props => props.children;
+
+export default aux;
