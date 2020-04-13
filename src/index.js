96843af44e38e2260b45cdccf00d0db893d7db77
anmolsukki <anmolsukki94@gmail.com>
2020-04-14 00:21:42 +0530

    useContext

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AuthContextProvider from "./context/auth-context";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
