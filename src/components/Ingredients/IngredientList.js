6a1914b495e91bbd662ec2b5676eb625df74c205
anmolsukki <anmolsukki94@gmail.com>
2020-04-14 00:48:15 +0530

    performance

--- src/components/Ingredients/IngredientList.js
+++ src/components/Ingredients/IngredientList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './IngredientList.css';
 
-const IngredientList = props => {
+const IngredientList = React.memo(props => {
   return (
     <section className="ingredient-list">
       <h2>Loaded Ingredients</h2>
@@ -15,6 +15,6 @@ const IngredientList = props => {
       </ul>
     </section>
   );
-};
+});
 
 export default IngredientList;
