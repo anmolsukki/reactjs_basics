5bf224de9a70e24569912e8592fb0821b68bd979
anmolsukki <anmolsukki94@gmail.com>
2020-04-13 20:33:27 +0530

    ref in functional component

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import classes from './Cockpit.css';
 
-const cockpit = ( props ) => {
+const Cockpit = ( props ) => {
+
+  const togglBtnRef = useRef(null);
+
+  useEffect(() => {
+    togglBtnRef.current.click();
+  }, [])
+
     const assignedClasses = [];
     let btnClass = '';
     if (props.showPersons) {
@@ -20,11 +27,11 @@ const cockpit = ( props ) => {
         <div className={classes.Cockpit}>
             <h1>{props.title}</h1>
             <p className={assignedClasses.join( ' ' )}>This is really working!</p>
-            <button
+            <button ref={togglBtnRef}
                 className={btnClass}
                 onClick={props.clicked}>Toggle Persons</button>
         </div>
     );
 };
 
-export default cockpit;
+export default Cockpit;
